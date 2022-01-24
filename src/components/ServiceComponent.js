2ae5297335e22d4c96c777554c9d0336e8be3af8
namvkim <sonnamttt@gmail.com>
2022-01-24 23:12:51 +0700

    services complete

--- src/components/ServiceComponent.js
+++ src/components/ServiceComponent.js
@@ -1,86 +1,94 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
-import { getDetail } from '../../services';
-import CenterContext from '../context/CenterContext';
+import React, { useContext, useEffect, useState } from "react";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { getDetail } from "../../services";
+import CenterContext from "../context/CenterContext";
 
-const ServiceComponent=({centerId})=> {
-    const [services,setServices] =useState([])
-    const centers = useContext(CenterContext);
-    useEffect(async ()=>{
-        setServices(getDetail(centers,centerId,"services")); 
-    },[]);
-    const renderItem = ({ item}) =>{
-        const {icon,title,description,time}=item
-        return  (
-            <View style={styles.service}>
-            <View style={styles.body}>
-                <Image
-                    source={{ uri: icon }}
-                    style={styles.icon}
-                />
-                <View style={styles.content}>
-                    <Text style={styles.title}>{title}</Text>
-                    <Text style={styles.subtitle}>{time}</Text>
-                    <Text style={styles.description}>{description}</Text>
-                </View>
-            </View>
-    
-        </View>
-        );
-    }
+const ServiceComponent = ({ centerId }) => {
+  const [services, setServices] = useState([]);
+  const centers = useContext(CenterContext);
+  useEffect(async () => {
+    setServices(getDetail(centers, centerId, "services"));
+  }, []);
+  const renderItem = ({ item }) => {
+    const { icon, title, description, time, price } = item;
     return (
-        <View>
-        {services.length?<FlatList
-            data={services}
-            renderItem={renderItem}
-            keyExtractor={(item,index) => `services${index}`}
-        />:<Text> Loading</Text>}
+      <View style={styles.service}>
+        <View style={styles.body}>
+          <Image source={{ uri: icon }} style={styles.icon} />
+          <View style={styles.content}>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.subtitle}>{time}</Text>
+            <Text style={styles.description}>
+              <Text style={styles.price}>${price}.00</Text> / {description}
+            </Text>
+          </View>
         </View>
-    )
-}
+      </View>
+    );
+  };
+  return (
+    <View style={styles.container}>
+      {services.length ? (
+        <FlatList
+          data={services}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => `services${index}`}
+          showsVerticalScrollIndicator={false}
+        />
+      ) : (
+        <Text> Loading</Text>
+      )}
+    </View>
+  );
+};
 
 export default ServiceComponent;
 
 const styles = StyleSheet.create({
-    service: {
-        flex: 1,
-        padding: 10,
+  container: {
+    flex: 1,
+  },
+  service: {
+    paddingHorizontal: 20,
+    paddingBottom: 16,
+  },
+  body: {
+    flexDirection: "row",
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-    body: {
-        flexDirection: 'row',
-        padding: 20,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 0,
-        elevation: 1,
-        backgroundColor: "white",
-        borderRadius: 10
-    },
-    content: {
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        paddingHorizontal: 16,
-    },
-    title: {
-        color: 'black',
-        paddingVertical: 4,
-        fontWeight: 'bold',
-        fontSize: 14,
-    },
-    subtitle: {
-        color: 'black',
-        paddingVertical: 4,
-    },
-    description: {
-        color: 'black',
-        paddingVertical: 4,
-    },
-    icon:{
-        height: 48,
-        width: 48,
-    }
-
-})
+    shadowOpacity: 0,
+    elevation: 1,
+    backgroundColor: "white",
+    borderRadius: 10,
+  },
+  content: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+  },
+  title: {
+    color: "black",
+    paddingVertical: 4,
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  subtitle: {
+    color: "black",
+    paddingVertical: 4,
+  },
+  description: {
+    color: "black",
+    paddingVertical: 4,
+  },
+  icon: {
+    height: 48,
+    width: 48,
+  },
+  price: {
+    fontWeight: "bold",
+  },
+});
